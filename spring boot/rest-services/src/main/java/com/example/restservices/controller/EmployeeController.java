9f5c7b540a227dd946821ed4be284e9d643264a9
haomingjue <haomingjue@outlook.com>
2022-09-18 21:45:55 -0400

    updated RESTful elements

--- spring boot/rest-services/src/main/java/com/example/restservices/controller/EmployeeController.java
+++ spring boot/rest-services/src/main/java/com/example/restservices/controller/EmployeeController.java
@@ -5,11 +5,17 @@ import com.example.restservices.payroll.EmployeeRepository;
 import com.example.restservices.payroll.LoadDatabase;
 import com.example.restservices.utils.EmployeeNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeController {
@@ -22,23 +28,32 @@ public class EmployeeController {
     // Aggregate root
     // tag::get-aggregate-root[]
     @GetMapping("/employees")
-    public ResponseEntity<?> all() {
-        return new ResponseEntity<>(repository.findAll(), HttpStatus.ACCEPTED);
+    CollectionModel<EntityModel<Employee>> all() {
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+                .map(employee -> EntityModel.of(employee,
+                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+                .collect(Collectors.toList());
+        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
     // end::get-aggregate-root[]
 
     @PostMapping("/employees")
     public ResponseEntity<?> newEmployee(@RequestBody Employee newEmployee) {
-        return new ResponseEntity<>(repository.save(newEmployee), HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(repository.save(newEmployee).toString() + "is Saved", HttpStatus.ACCEPTED);
     }
 
     // Single item
 
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
+    EntityModel<Employee> one(@PathVariable Long id) {
 
-        return repository.findById(id)
+        Employee employee = repository.findById(id) //
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return EntityModel.of(employee, //
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
     }
 
     @PutMapping("/employees/{id}")
